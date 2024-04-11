953e2ed901095687d0c76546c6b255da555ac4a3
Nicolas Wrobel <nicolas.18.wrobel@gmail.com>
2024-04-11 14:46:31 +0200

    ConfigServer added to project

---
+++ ConfigServer/src/test/java/fr/insa/fisa/archi/ArchiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.fisa.archi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

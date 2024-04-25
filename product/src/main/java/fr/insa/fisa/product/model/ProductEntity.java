6b8b4c8c560a81d7d908f5871534ab2e989a7f3a
Damien-Collot <dcollot76@gmail.com>
2024-04-25 15:24:52 +0200

    Fix repair product

--- product/src/main/java/fr/insa/fisa/product/model/ProductEntity.java
+++ product/src/main/java/fr/insa/fisa/product/model/ProductEntity.java
@@ -2,18 +2,52 @@ package fr.insa.fisa.product.model;
 
 import jakarta.persistence.*;
 
-import java.util.Date;
-
 @Entity
 @Table(name = "product")
 public class ProductEntity {
     @Id
-    @GeneratedValue
-    Long Id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // ou GenerationType.AUTO selon la BDD
+    private Long id;
+
+    @Column(name = "name") // Si le nom de la colonne est différent, spécifie-le ici
+    private String name;
+
     @Column
-    String name;
-    @Column
-    String description;
-    @Column
-    float montant;
+    private String description;
+
+    @Column(name = "montant")
+    private float montant;
+
+    // Getters et Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getMontant() {
+        return montant;
+    }
+
+    public void setMontant(float montant) {
+        this.montant = montant;
+    }
 }

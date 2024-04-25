1b4f6a33c1bf5dc0093cb670b303bc8fabd408f5
Matheo H-M <matheo.herbin.mairesse@gmail.com>
2024-04-25 17:08:12 +0200

    Feat: création d'order

---
+++ order/src/main/java/fr/insa/fisa/order/Model/OrderEntity.java
@@ -0,0 +1,77 @@
+package fr.insa.fisa.order.Model;
+
+import com.ctc.wstx.shaded.msv_core.datatype.xsd.datetime.IDateTimeValueType;
+import jakarta.persistence.*;
+import jdk.jfr.DataAmount;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name="order")
+public class OrderEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int idClient;
+    private String reference;
+    private LocalDate date;
+    private String libelle;
+    private int idProduit;
+    private int quantite;
+
+    public OrderEntity(int id, int idClient, String reference, LocalDate date, String libelle, int idProduit, int quantite) {
+        this.id = id;
+        this.idClient = idClient;
+        this.reference = reference;
+        this.date = date;
+        this.libelle = libelle;
+        this.idProduit = idProduit;
+        this.quantite = quantite;
+    }
+
+    public OrderEntity() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(int idClient) {
+        this.idClient = idClient;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+}
